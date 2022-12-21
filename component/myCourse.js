9753c2918b133e27fbf648e8199265e7b4570820
mohammad <mohammadbibakiyan@gmail.com>
2022-12-22 00:52:26 +0330

    get data  from backend for user course and user information

--- component/myCourse.js
+++ component/myCourse.js
@@ -1,27 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Card from "./../component/card"
 
 const MyCourse=()=>{
     const [courses,setCourses]=useState([]);
-    const [loading,setLoading]=useState(false);
+    const [loading,setLoading]=useState(true);
+
+    useEffect(()=>{
+        const fetchData = async () => {
+          const response = await fetch(
+            `http://127.0.0.1:4000/api/v1/user/myCourse`,
+            {
+              method: "GET",
+              headers: { "Content-Type": "application/json" },
+              credentials: "include",
+            }
+          );
+          const result = await response.json();
+          setCourses(result.data);
+        }
+        fetchData().catch((err)=>console.log(err))
+        setLoading(false);
+      },[])
+
     return(
         <>
             <div className="max-w-[1280px] mx-auto card p-4 mb-5">
                 <h3 className="text-xl text-[#31344b]">دوره های من</h3>
                 <hr className="mb-10"/>
-                <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+                <div className="grid  grid-cols-1 lg:grid-cols-2  gap-4">
                     {loading&&<p>... loading</p>}
-                    {courses?.length===0&&!loading&&<p className="text-sm">شما تا کنون دوره ای ثبت نکرده اید</p>}
-                    {courses?.length>0&&!loading&&courses.map(e=><Card key={e._id} image={e.photo} _id={e._id} teacher={e.user.fullName} title={e.title} description={e.description}/>)}
+                    {courses?.myCourse?.length===0&&!loading&&<p className="text-sm">شما تا کنون دوره ای ثبت نکرده اید</p>}
+                    {courses?.myCourse?.length>0&&!loading&&courses.myCourse.map(e=><Card key={e._id} image={e.photo} _id={e._id} teacher={e.user.fullName} title={e.title} description={e.description}/>)}
                 </div>
             </div>
             <div className="max-w-[1280px] mx-auto card p-4 mb-5">
                 <h3 className="text-xl text-[#31344b]">دوره های ثبت نام شده</h3>
                 <hr className="mb-10"/>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+                <div className="grid grid-cols-1  lg:grid-cols-2  gap-4">
                     {loading&&<p>... loading</p>}
-                    {courses?.length===0&&!loading&&<p className="text-sm">شما تا کنون در دوره ای شرکت نکرده اید</p>}
-                    {courses?.length>0&&!loading&&courses.map(e=><Card key={e._id} image={e.photo} _id={e._id} teacher={e.user.fullName} title={e.title} description={e.description}/>)}
+                    {courses?.RegisteredCourses?.length===0&&!loading&&<p className="text-sm">شما تا کنون در دوره ای شرکت نکرده اید</p>}
+                    {courses?.RegisteredCourses?.length>0&&!loading&&courses.RegisteredCourses.map(e=><Card key={e._id} image={e.photo} _id={e._id} teacher={e.user.fullName} title={e.title} description={e.description}/>)}
                 </div>
             </div>
         </>
--- component/personalInfo.js
+++ component/personalInfo.js
@@ -1,6 +1,51 @@
+import { useEffect, useState } from "react";
+
 const PersonalInfo=()=>{
+    const [info,setInfo]=useState(null);
+    const [loading,setLoading]=useState(true);
+    useEffect(()=>{
+        const fetchData = async () => {
+          const response = await fetch(
+            `http://127.0.0.1:4000/api/v1/user/myPersonalInfo`,
+            {
+              method: "GET",
+              headers: { "Content-Type": "application/json" },
+              credentials: "include",
+            }
+          );
+          const result = await response.json();
+          setInfo(result.data);
+        }
+        fetchData().catch((err)=>console.log(err))
+        setLoading(false);
+      },[]);
     return(
-        <div className="card">اطلاعات کاربر</div>
+        <div className="max-w-[1280px] mx-auto card p-4 mb-5 text-[#31344b]">
+                <h3 className="text-xl text-[#31344b]">اطلاعات کاربری</h3>
+                <hr className="mb-10"/>
+                <div>
+                    {loading&&<p>... loading</p>}
+                    {!info===0&&!loading&&<p className="text-sm">اطلاعات کاربری یافت نشد</p>}
+                    {info&&!loading&&<div className="grid gap-5 grid-cols1 md:grid-cols-2">
+                        <div>
+                            <p className="text-sm mb-1">نام و نام خانوادگی :</p>
+                            <p>{info.fullName}</p>  
+                        </div>  
+                        <div>
+                            <p className="text-sm mb-1">ایمیل :</p>
+                            <p>{info.email}</p>  
+                        </div>    
+                        <div>
+                            <p className="text-sm mb-1">مهارت ها :</p>
+                            <p>{info?.skils?.map(e=><p>{e}</p>)||"مهارتی ثبت نشده است"}</p>  
+                        </div>    
+                        <div>
+                            <p className="text-sm mb-1">معرفی :</p>
+                            <p>{info.bio||"متن معرفی ثبت نشده است"}</p>  
+                        </div>    
+                    </div>}
+                </div>
+            </div>
     )
 };
 
